cfc1b602a990a2c4c654e66a6e7cdb6f212a03a5
Gabriel Ulrich <ulrichgw06@mounties.mansfield.edu>
2017-02-08 21:52:50 -0600

    Another commit, update changes

---
+++ src/Week3Folder/ChessUI.java
@@ -0,0 +1,13 @@
+
+package Week3Folder;
+
+/**
+ *
+ * @author Administrator Gabe
+ */
+public class ChessUI {
+    public static void main(String[] args) {
+        ChessBoard chess = new ChessBoard();
+        
+    }
+}
