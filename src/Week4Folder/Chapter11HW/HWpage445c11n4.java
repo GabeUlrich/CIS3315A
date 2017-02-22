affd285a7c49d4696b95b240acdd365a818c7a0a
Gabriel Ulrich <ulrichgw06@mounties.mansfield.edu>
2017-02-22 07:56:13 -0600

    update

--- src/Week4Folder/Chapter11HW/HWpage445c11n4.java
+++ src/Week4Folder/Chapter11HW/HWpage445c11n4.java
@@ -1,32 +1,44 @@
 package Week4Folder.Chapter11HW;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
+import java.util.Scanner;
 
 /**
  *
  *
- * @author Administrator
+ * @author Administrator Gabe - solution by Profphilips
  */
 public class HWpage445c11n4 {
 
     public static Integer max(ArrayList<Integer> list) {
+        int biggest;
         if (list.size() > 0) {
-            int max = list.get(0).intValue();
+            biggest = list.get(0);
             for (int i = 1; i < list.size(); i++) {
-            if (list.get(i) > max) {
-                    max = list.get(i);
+                if (list.get(i) > biggest) {
+                    biggest = list.get(i);
                 }
-
             }
-            return max;
+            return biggest;
         }
         return null;
     }
 
     public static void main(String[] args) {
-        int[] array = {5, 6, 4};
-        ArrayList<Integer> list = new ArrayList(Arrays.asList(array));
+        //Integer[] array = {5, 6, 4};
+        //ArrayList<Integer> list = new ArrayList(Arrays.asList(array));
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList();
+        System.out.print("Enter a number (end with 0): ");
+        while (sc.hasNext()) {
+            int num = Integer.parseInt(sc.nextLine());
+            if (num == 0) {
+                break;
+            }
+            list.add(num);
+            System.out.print("Enter a number (end with 0): ");
+        }
         System.out.println("Max is " + max(list));
     }
 }
--- src/Week4Folder/Chapter11HW/HWpage447c11n11.java
+++ src/Week4Folder/Chapter11HW/HWpage447c11n11.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Week4Folder.Chapter11HW;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
 /**
  *
- * @author Administrator
+ * @author Administrator Gabe - solution by Profphilips
  */
 public class HWpage447c11n11 {
-    
+    public static void sort(ArrayList<Integer> list) {
+        // one way is to use a lambda expression
+        // list.sort((integer, anotherInteger) -> integer.compareTo(anotherInteger));
+        //
+        // I came across this simpler technique on stackoverflow at
+        // http://stackoverflow.com/questions/23701943/sorting-arraylist-with-lambda-in-java-8
+        list.sort(Comparator.naturalOrder());
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList();
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            list.add(Integer.parseInt(sc.nextLine()));
+        }
+        sort(list);
+        System.out.println("Sorted list is " + list.toString());
+}
 }
