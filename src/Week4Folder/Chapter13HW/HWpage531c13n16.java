c81d2fa927d93ee753981c5fb7f9268e32b7cb50
Gabriel Ulrich <ulrichgw06@mounties.mansfield.edu>
2017-02-27 07:46:00 -0600

    Updates

--- src/Week4Folder/Chapter13HW/HWpage531c13n16.java
+++ src/Week4Folder/Chapter13HW/HWpage531c13n16.java
@@ -1,61 +1,105 @@
 package Week4Folder.Chapter13HW;
 
+import static Week4Folder.Chapter13HW.Rational.parseExpression;
 import java.util.ArrayList;
 
 /**
  *
  *
- * @author Administrator Gabe
+ * @author Administrator Gabe - Solution ProfPhillips
  */
 public class HWpage531c13n16 {
 
     public static void main(String[] args) {
+        String exp1 = "3/4 + 1/5";
+        String exp2 = "3/4 - 1/5";
+        String exp3 = "3/4 * 1/5";
+        System.out.println(exp1 + " = " + parseExpression(exp1));
+        System.out.println(exp2 + " = " + parseExpression(exp2));
+        System.out.println(exp3 + " = " + parseExpression(exp3));
+    }
+}
 
-        String[] token = getTokens(args);
+/**
+ * Class to work with rational numbers.
+ *
+ * @author John Phillips
+ */
+class Rational {
 
-        if (token.length != 3 || !isValidOperator(token)) {
-            System.out.println("Invalid arguments.");
-            System.out.println("Usage: java Chapter_13.Exercise_16 1/2 * 8/17");
-            System.exit(1);
-        }
+    int numerator;
+    int denominator;
 
-        BigRational operand1 = new BigRational(token[0]);
-        BigRational operand2 = new BigRational(token[2]);
-
-        BigRational answer = null;
-        switch (args[1]) {
-            case "+": answer = operand1.add(operand2); break;
-            case "-": answer = operand1.subtract(operand2); break;
-            case "*": answer = operand1.multiply(operand2); break;
-            case "/": answer = operand1.divide(operand2); break;
-        }
-
-        System.out.println(operand1 + " " + args[1] + " " + operand2 + " = " + answer);
+    /**
+     * Construct new Rational from String in form of "3/4".
+     *
+     * @param s
+     */
+    public Rational(String s) {
+        String[] sArray = s.split("/");
+        numerator = Integer.parseInt(sArray[0]);
+        denominator = Integer.parseInt(sArray[1]);
     }
 
-    private static boolean isValidOperator(String[] token) {
-
-        return  token[0].matches("[\\d]*[0-9]/[\\d]*[0-9]") &&
-                token[1].matches("[\\-\\+\\*\\/]") &&
-                token[2].matches("[\\d]*[0-9]/[\\d]*[0-9]");
+    public Rational(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
-    private static String[] getTokens(String[] strings) {
-        String merged = " ";
-        for (String s : strings) {
-            merged += " " + s;
+    /**
+     * Process expression in form of "3/4 + 1/5".
+     *
+     * @param s
+     * @return
+     */
+    public static Rational parseExpression(String s) {
+        String[] sArray = s.split(" ");
+        Rational op1 = new Rational(sArray[0]);
+        String operator = sArray[1];
+        Rational op2 = new Rational(sArray[2]);
+        if (operator.equals("+")) {
+            return op1.add(op2);
+        } else if (operator.equals("-")) {
+            return op1.subtract(op2);
+        } else if (operator.equals("*")) {
+            return op1.multiply(op2);
         }
+        return null;
+    }
 
-        String[] tokens = merged.split("[\\s ]");
-        ArrayList<String> temp = new ArrayList<>();
-        // remove blanks
-        for (String s : tokens) {
-            if (s.length() != 0) {
-                temp.add(s);
-            }
+    public Rational add(Rational ra) {
+        int commonDenominator = this.denominator;
+        int num1Cross = this.numerator;
+        int num2Cross = ra.numerator;
+        if (this.denominator != ra.denominator) {
+            commonDenominator = this.denominator * ra.denominator;
+            num1Cross = this.numerator * ra.denominator;
+            num2Cross = ra.numerator * this.denominator;
         }
-        return temp.toArray(new String[temp.size()]);
+        return new Rational((num1Cross + num2Cross), commonDenominator);
+    }
+
+    public Rational subtract(Rational ra) {
+        int commonDenominator = this.denominator;
+        int numCross1 = this.numerator;
+        int numCross2 = ra.numerator;
+        if (this.denominator != ra.denominator) {
+            commonDenominator = this.denominator * ra.denominator;
+            numCross1 = this.numerator * ra.denominator;
+            numCross2 = ra.numerator * this.denominator;
+        }
+        return new Rational((numCross1 - numCross2), commonDenominator);
+    }
+
+    public Rational multiply(Rational ra) {
+        int top = this.numerator * ra.numerator;
+        int bottom = this.denominator * ra.denominator;
+        return new Rational(top, bottom);
+    }
+
+    @Override
+    public String toString() {
+        return "" + numerator + "/" + denominator;
     }
 
 }
-
